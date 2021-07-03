9282c35083b1897918fc18d258ebc5573aa238e1
Bryan Daniel <brdaniel117@gmail.com>
2021-07-02 21:22:43 -0700

    started writing code for index file

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,41 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const readmeInfo = () => {
+    return inquirer.prompt({
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'What is your Project Title? (Required)',
+            validate: projectTitleInput => {
+              if (projectTitleInput) {
+                return true;
+              } else {
+                console.log('Please enter your Project Title!');
+                return false;
+              }
+            }
+          },{
+            type: 'input',
+            name: 'projectDescription',
+            message: 'Enter a description about your project? (Required)',
+            validate: projectTitleInput => {
+              if (projectTitleInput) {
+                return true;
+              } else {
+                console.log('Please enter a breif description about your projet!');
+                return false;
+              }
+            }
+          },{
+            type: 'confirm',
+            name: 'confirmInstallation',
+            message: 'Does your project require any additonal "Installation" instructions?',
+            default: true
+          }
+    })
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
